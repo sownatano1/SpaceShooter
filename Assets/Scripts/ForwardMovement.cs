f4b91f877e7181dc0c832ded88a3b7390013d014
sownatano1 <jonathandahre@gmail.com>
2025-10-15 18:54:25 +0200

    Update
    Added shooting and Bounds

---
+++ Assets/Scripts/ForwardMovement.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class ForwardMovement : MonoBehaviour
+{
+    public float speed = 5;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,11 @@ public class PlayerController : MonoBehaviour
     public float shipSpeed = 10f;
     public float horizontalInput;
     public float verticalInput;
+
+    public float borderX = 10;
+    public float borderY = 8;
+
+    public GameObject projectilePrefab;
     void Start()
     {
 
@@ -16,14 +21,15 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        //Inputs
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
 
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
         // Båda inputs bli en vector "movement"
         Vector3 movement = new Vector3(horizontalInput, verticalInput, 0);
 
-        // Normaliserar rörelse så det bli på samma hastighet.
         if (movement.magnitude > 1)
         {
             movement = movement.normalized;
@@ -31,5 +37,33 @@ public class PlayerController : MonoBehaviour
 
         // Utför rörelsen med jämn hastighet
         transform.Translate(movement * Time.deltaTime * shipSpeed);
+
+        //X Border
+        if (transform.position.x > borderX)
+        {
+            transform.position = new Vector3(borderX , transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x < -borderX)
+        {
+            transform.position = new Vector3(-borderX, transform.position.y, transform.position.z);
+        }
+
+        //Y Border
+        if (transform.position.y > borderY)
+        {
+            transform.position = new Vector3(transform.position.x, borderY, transform.position.z);
+        }
+
+        if (transform.position.y < -borderY)
+        {
+            transform.position = new Vector3(transform.position.x, -borderY, transform.position.z);
+        }
+
+        //Projectiles
+        if (Input.GetKeyDown(KeyCode.Space))    
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
