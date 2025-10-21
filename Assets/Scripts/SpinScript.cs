fe29a6c56b7622edcc9a62f9686ef8be80266205
sownatano1 <jonathandahre@gmail.com>
2025-10-22 00:31:07 +0200

    Spin Enemy

---
+++ Assets/Scripts/SpinScript.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.InputSystem.Controls;
+
+public class SpinScript : MonoBehaviour
+{
+    public float spinSpeed = 1f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * spinSpeed * Time.deltaTime);
+    }
+}
