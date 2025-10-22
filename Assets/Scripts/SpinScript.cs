f308796dca4e17937b083ee41f74e49c7560a1e8
sownatano1 <jonathandahre@gmail.com>
2025-10-22 12:51:51 +0200

    EnemyTripleGun

--- Assets/Scripts/SpinScript.cs
+++ Assets/Scripts/SpinScript.cs
@@ -4,6 +4,7 @@ using UnityEngine.InputSystem.Controls;
 public class SpinScript : MonoBehaviour
 {
     public float spinSpeed = 1f;
+    public bool rotateXAxis = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,6 +14,14 @@ public class SpinScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(Vector3.forward * spinSpeed * Time.deltaTime);
+        if (rotateXAxis == false)
+        {
+            transform.Rotate(Vector3.forward * spinSpeed * Time.deltaTime);
+        }
+
+        if (rotateXAxis == true)
+        {
+            transform.Rotate(Vector3.right * spinSpeed * Time.deltaTime);
+        }
     }
 }
